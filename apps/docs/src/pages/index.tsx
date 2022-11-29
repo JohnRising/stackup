d631cb52f93e0da4f85cefbf80390d03edee5dd4
John Rising <johnrising@Johns-MacBook-Air.local>
2022-11-28 16:40:35 -0800

    add quickstart guide

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/introduction"
+            to="/docs/guides/quickstart"
           >
             Get Started
           </Link>
